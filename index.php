1a64586097a9685e8c3695cdc844f2d8314e0d00
Rafaelcostadelima <rafita.limagames2@gmail.com>
2026-04-30 12:09:27 -0300

    Adição de imagem, teste

--- index.php
+++ index.php
@@ -18,9 +18,11 @@ echo "Conectado com sucesso ao banco de dados!";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teste de </title>
+    <title>Conexão com Banco de Dados</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" type="image/png" href="img/logo.png">
 </head>
 <body>
-    
+
 </body>
 </html>
