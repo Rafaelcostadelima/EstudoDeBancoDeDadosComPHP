8a0244fab940737ca77fa4e97cde33f61394a5b7
Rafaelcostadelima <rafita.limagames2@gmail.com>
2026-05-08 08:23:52 -0300

    Código quase todo comentado

--- index.php
+++ index.php
@@ -1,25 +1,26 @@
 <?php
-$host = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "meu_sistema";
+// Aqui, estamos preparando o ambiente para poder acessar o banco de dados
+$host = "localhost"; //Padrão do XAMPP
+$usuario = "root"; //Usuário padrão do XAMPP
+$senha = ""; //Senha padrão do XAMPP
+$banco = "meu_sistema"; //Nome escolhido para o banco de dados
 
-$conn = mysqli_connect($host, $usuario, $senha, $banco);
+$conn = mysqli_connect($host, $usuario, $senha, $banco); //Aqui é uma firmação da conexão, o banco de dados está se conectando, não basta apenas ter o $host e essas coisas
 
-// Verificar se houve erro
+// Verifica se houve erro, caso tenha, ele indica um problema com a conexão
 if (!$conn) {
     die("Falha na conexão: " . mysqli_connect_error());
 }
 
-$mensagem = "";
+$mensagem = ""; //Define uma mensagem em branco para usarmos mais tarde
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST['email'];
-    $user = $_POST['username'];
-    $pass = password_hash($_POST['senha'], PASSWORD_DEFAULT);
-    $sql = "INSERT INTO usuarios (email, username, senha) VALUES (?, ?, ?)";
-    $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, "sss", $email, $user, $pass);
+if ($_SERVER["REQUEST_METHOD"] == "POST") { //Guardando as informações sobre o servidor e sobre quem, está acessando ele. Com este "= 'POST'" a gente delimita para fazer a ação abaixo apenas quando o método for "POST", que é quando o usuário clica no botão
+    $email = $_POST['email']; //Pega o que foi digitado no input com o 'name="email"'
+    $user = $_POST['username']; //Pega o que foi digitado no input com o "name='username'"
+    $pass = password_hash($_POST['senha'], PASSWORD_DEFAULT); //Este código criptografa a senha digitada pelo usuário
+    $sql = "INSERT INTO usuarios (email, username, senha) VALUES (?, ?, ?)"; //Insira dentro da tabela de usuários. Dentro do "(email, username, senha)", indicamos a exata coluna que queremos pra onde os dados vão. O "VALUES (?, ?, ?)" serve para segurança. Com as interrogações, o PHP entende o input estritamente como um texto comum, então os hackers não conseguem burlar esse sistema
+    $stmt = mysqli_prepare($conn, $sql); //Aqui, o código pede para que a conexão interceda com o banco de dados, e enviando os dados que inserimos na linha anterior. Aqui, o banco de dados já se prepara para receber os dados que entregaremos para ele nas próximas linhas
+    mysqli_stmt_bind_param($stmt, "sss", $email, $user, $pass); //Aqui, o código finalmente envia os dados corretos para cada espaço reservado que codamos na linha 21. O 'sss' significa que armazenaremos 3 strings, é uma etapa final de segurança do PHP. O PHP entende que tudo o que foi colocado dentro dos valores são apenas strings, então códigos maliciosos dos hackers não terão efeito quando inseridos no formulário
     if (mysqli_stmt_execute($stmt)) {
         $mensagem = "Usuário cadastrado com suscesso";
     } else {
