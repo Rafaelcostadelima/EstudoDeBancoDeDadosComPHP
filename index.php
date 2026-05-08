f38294c36d998067c722b378f322cb64efb12315
Rafito3080 <rafita.limagames@gmail.com>
2026-05-07 21:10:16 -0300

    Versão final do github - testes finalizados

--- index.php
+++ index.php
@@ -39,7 +39,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
     <div class="container">
         <h2>Criar uma Conta</h2>
-        <?php echo"Tentativa de merge com a origin main (teste rápido)"?>
     </div>
 </body>
 </html>
